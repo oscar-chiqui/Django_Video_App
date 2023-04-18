a33bd5a0a33260d2a62a2b6f75e39796f67dc60e
Oscar  A. Chiqui <anibalchiqui88@hotmail.com>
2023-04-18 00:31:01 -0500

    Forms and URL validation test

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import VideoForm
+
+
 
 # Create your views here.
+
+def home(request):
+    app_name = 'Travel Agency' # destinations company 
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+def add(request):
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'Video added successfully!')
+
+            # Show a message to the user about the list of videos 
+        else: 
+            messages.warning(request, 'Please check the data entered.')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+        
+        
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
