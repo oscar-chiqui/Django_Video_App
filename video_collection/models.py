1eb9948405fab62744bc725c153b41e3244fe089
Oscar  A. Chiqui <anibalchiqui88@hotmail.com>
2023-04-18 12:44:20 -0500

    redirect video_list

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,11 +1,33 @@
-from django.db import models
+from urllib import parse
+from django.db import models 
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Extract the video id from a youtube url
+
+        if not self.url.startswith('https://www.youtube.com/watch'):
+            raise ValidationError(f'Not a Youtube URL {self.url}')
+
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query  # 'v=12345678'
+        if not query_string:
+            raise ValidationError(f'Invalid Youtube URL {self.url}')
+        parameters = parse.parse_qs(query_string, strict_parsing=True) #dictionary
+        v_parameters_list = parameters.get('v') #return None if no key found.
+        if not v_parameters_list:      # Checking if None or empty list
+            raise ValidationError(f'Invalid Youtube URL, missing parameters {self.url}')
+        self.video_id = v_parameters_list[0]  # String
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video_id}, Notes: {self.notes[:200]}'
 
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from .forms import VideoForm
+from .models import Video
 
 
 
@@ -14,15 +17,28 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'Video added successfully!')
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'Video added successfully!')
+
 
             # Show a message to the user about the list of videos 
-        else: 
-            messages.warning(request, 'Please check the data entered.')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            except ValidationError:
+                messages.warning(request, 'Invalid Youtube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        
+        messages.warning(request, 'Please check the data entered.')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
         
         
 
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
+
+
